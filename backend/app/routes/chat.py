da93d8b625ed3edf034665e179d7a60689b14998
George Balch <george.balch@proton.me>
2023-09-16 02:05:43 -0500

    Use fine-tuned model for chat response

--- backend/app/routes/chat.py
+++ backend/app/routes/chat.py
@@ -23,8 +23,6 @@ from app.models.completion import Model
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = ChatOpenAI()
-
 class CompletionRequest(BaseModel):
     prompt: str
     section: str
@@ -51,6 +49,8 @@ async def get_chat_response(
     human_template = "{sentence}"
     human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    llm = ChatOpenAI()
     chain = LLMChain(
         llm=llm,
         prompt=chat_prompt,
@@ -74,13 +74,15 @@ async def get_chat_response(
                 "response": response,
             }
         case "Yes":
-            system_template = """You are a young Colombian woman talking to her boyfriend.  Return a response as if you are holding a conversation."""
-            system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+            model = await Model.find_one(
+                Model.section == completionRequest.section,
+                Model.language == completionRequest.language,
+            )
             human_template = "{sentence}"
             human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-            chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+            chat_prompt = ChatPromptTemplate.from_messages([human_message_prompt])
             chain = LLMChain(
-                llm=llm,
+                llm=ChatOpenAI(model=model.name),
                 prompt=chat_prompt,
             )
             response = chain.run(sentence = completionRequest.prompt, language=completionRequest.language)
@@ -90,13 +92,3 @@ async def get_chat_response(
             }
         case _:
             raise Exception("Chat model did not return a valid response.")
-    # model = await Model.find_one(Model.section == completionRequest.section, Model.language == completionRequest.language)
-    # completion = openai.ChatCompletion.create(
-    #     model=model.name,
-    #     messages=[
-    #         {"role": "user", "content": completionRequest.prompt}
-    #     ]
-    # )
-    # return {
-    #     "response": completion.choices[0].message.content,
-    # }
