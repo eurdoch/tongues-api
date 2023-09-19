76a7bda30ccbcdeb59fe4f5b232492889a558a32
George Balch <george.balch@proton.me>
2023-09-19 04:45:45 +0000

    Change /suggestions to return json directly from GPT

--- backend/app/routes/suggestion.py
+++ backend/app/routes/suggestion.py
@@ -6,8 +6,9 @@ from fastapi import (
 )
 from typing import Union
 import os
+import json
 
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import (
     SystemMessagePromptTemplate,
@@ -36,15 +37,14 @@ async def get_suggestions(
     language: str, # Query
     prompt: Union[str, None] = None, # Query
 ):
-    llm = OpenAI(model='gpt-3.5-turbo-instruct')
+    llm = ChatOpenAI()
     if prompt == None:
         system_template = """You are a {language} translator who gives suggestions
-        for sentences to use in conversation.
-        ONLY return a comma separated list of the sentences.
+        for sentences to use in conversation. The user will input a language and you will
+        return three sentences for starting a basic conversation in that language.
+        ONLY return a json object with the key suggestions.
         """
-        human_template = """
-        Give me three sentences for starting a conversation in {language}.
-        """
+        human_template = "{language}"
         system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
         human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
         chat_prompt = ChatPromptTemplate.from_messages([
@@ -55,6 +55,5 @@ async def get_suggestions(
             llm=llm,
             prompt=chat_prompt
         )
-        return {
-            "response": chain.run(language=language),
-        }
+        response = chain.run(language=language)
+        return json.loads(response.replace('\n', ''))
