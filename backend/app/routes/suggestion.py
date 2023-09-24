b37b5dcd03ecf512a4656c533d5a45b715343a90
George Balch <george.balch@proton.me>
2023-09-24 15:58:06 -0500

    Use OpenAI function calling to force json output

--- backend/app/routes/suggestion.py
+++ backend/app/routes/suggestion.py
@@ -5,15 +5,8 @@ from fastapi import (
 import json
 from pydantic import BaseModel
 
-from langchain.chains import LLMChain
-from langchain.llms import OpenAI
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-)
+import openai
 
-from app.app import llm
 from app.utils.auth import is_authorized
 from app.utils.chat import get_suggestions_by_language
 
@@ -29,8 +22,6 @@ class SuggestionRequest(BaseModel):
     language: str
     history: str = None
 
-instruct_llm = OpenAI(model="gpt-3.5-turbo-instruct")
-
 @router.post(
     "/suggestions"
 )
@@ -38,47 +29,53 @@ async def get_suggestions(
     suggestionRequest: SuggestionRequest,
 ):
     if suggestionRequest.history == None:
-        system_template = """You are a {language} translator who gives suggestions
-        for sentences to use in conversation. The user will input a language and you will
-        return three simple sentences under four words for initiating a conversation 
-        in that language.
-        ONLY return a json string with the key suggestions.
-        """
-        human_template = "{language}"
-        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        chat_prompt = ChatPromptTemplate.from_messages([
-            system_message_prompt,
-            human_message_prompt
-        ])
-        chain = LLMChain(
-            llm=llm,
-            prompt=chat_prompt
-        )
-        response = chain.run(language=suggestionRequest.language)
-        return json.loads(response.replace('\n', ''))
+        pass
+        # system_template = """You are a {language} translator who gives suggestions
+        # for sentences to use in conversation. The user will input a language and you will
+        # return three simple sentences under four words for initiating a conversation 
+        # in that language.
+        # ONLY return a json string with the key suggestions.
+        # """
+        # human_template = "{language}"
+        # system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+        # human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+        # chat_prompt = ChatPromptTemplate.from_messages([
+        #     system_message_prompt,
+        #     human_message_prompt
+        # ])
+        # chain = LLMChain(
+        #     llm=llm,
+        #     prompt=chat_prompt
+        # )
+        # response = chain.run(language=suggestionRequest.language)
+        # return json.loads(response.replace('\n', ''))
     else:
-        system_template = """You are a {language} translator who gives suggestions
-        for sentences to use in conversation. The user will input a conversation and you will
-        return three simple sentences as suggestions that the Human would respond with.
-        ONLY return a json string with the key suggestions and the value as list of the 
-        suggestions.
+        prompt = f"""You are a English {suggestionRequest.language} who gives suggestions for sentences to 
+        use in conversation. The user will input a conversation and you will return three 
+        simple sentences as suggestions that the Human would respond with.
+        ONLY return a json string with the key suggestions and the value as list of the suggestions.
+        
+        Conversation: {suggestionRequest.history}
         """
-        human_template = "{history}"
-        print(suggestionRequest.history)
-        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        chat_prompt = ChatPromptTemplate.from_messages([
-            system_message_prompt,
-            human_message_prompt
-        ])
-        chain = LLMChain(
-            llm=instruct_llm,
-            prompt=chat_prompt
+        suggestion_functions = [
+            {
+                'name': 'extract_suggestions',
+                'parameters': {
+                    'type': 'object',
+                    'properties': {
+                        'suggestions': {
+                            'type': 'array',
+                            'items': {
+                                'type': 'string'}
+                            }
+                    }
+                }
+            }
+        ]
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[{"role": "user", "content": prompt}],
+            functions=suggestion_functions,
+            function_call="auto",
         )
-        response = chain.run(
-            language=suggestionRequest.language, 
-            history=suggestionRequest.history
-        )
-        print(response)
         return json.loads(response.replace('\n', ''))
