59c3a14584b9a4e4cf729ca65633c5a9e2747d53
George Balch <george.balch@proton.me>
2023-09-24 04:34:18 +0000

    DOES NOT BUILD

--- backend/app/routes/suggestion.py
+++ backend/app/routes/suggestion.py
@@ -27,19 +27,19 @@ router = APIRouter(
     dependencies=[Depends(is_authorized)],
 )
 
-# Change to use query paramters instead of Path
-@router.get(
+@router.post(
     "/suggestions"
 )
 async def get_suggestions(
-    language: str, # Query
-    prompt: Union[str, None] = None, # Query
+    language: str,
+    history: str,
+    prompt: Union[str, None] = None,
 ):
     if prompt == None:
         system_template = """You are a {language} translator who gives suggestions
         for sentences to use in conversation. The user will input a language and you will
         return three simple sentences under four words for starting a conversation in that language.
-        ONLY return a json object with the key suggestions.
+        ONLY return a json string with the key suggestions.
         """
         human_template = "{language}"
         system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
@@ -56,12 +56,12 @@ async def get_suggestions(
         return json.loads(response.replace('\n', ''))
     else:
         system_template = """You are a {language} translator who gives suggestions
-        for sentences to use in conversation. The user will input a sentence and you will
-        return three simple sentences as suggestions to respond with.
+        for sentences to use in conversation. The user will input a history of sentences 
+        and you will return three simple sentences as suggestions to respond with.
         ONLY return a json string with the key suggestions and the value as list of the 
         suggestions.
         """
-        human_template = "{sentence}"
+        human_template = "{history}"
         system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
         human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
         chat_prompt = ChatPromptTemplate.from_messages([
@@ -72,6 +72,6 @@ async def get_suggestions(
             llm=llm,
             prompt=chat_prompt
         )
-        response = chain.run(language=language, sentence=prompt)
+        response = chain.run(language=language, history=history)
         print(response)
         return json.loads(response.replace('\n', ''))
