e375679cd79c2240ac3a1e75fb9e1445b6ccc644
George Balch <george.balch@proton.me>
2023-10-27 04:17:42 +0000

    Add check for subscription status in authorization call

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,10 +1,16 @@
 from app.auth import Auther
-from app.models.user import User
 from fastapi import HTTPException, Header
 from typing import Annotated
+from pydantic import BaseModel
 from firebase_admin import auth
+from app.models.user import User
+
+class UserSubscriptionStatus(BaseModel):
+    subscription: str
 
 async def is_authorized(authorization: Annotated[str, Header()]):
     token = authorization.split(' ')[1]
     decoded_token = auth.verify_id_token(token)
-    uid = decoded_token['uid']
+    user = await User.find_one(User.firebase_user_id == decoded_token['uid']).project(UserSubscriptionStatus)
+    if (user.subscription == 'inactive'):
+        raise HttpException(401)
