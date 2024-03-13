d3a7b2b952c80c01b3f0cf47f4eb513bbf3b25c6
George Balch <george.balch@proton.me>
2024-03-12 17:30:45 -0700

    Add email verification to authorized check

--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -1,7 +1,10 @@
-from fastapi import Header
+from fastapi import Header, HTTPException
 from typing import Annotated
 from firebase_admin import auth
 
 async def is_authorized(authorization: Annotated[str, Header()]):
     token = authorization.split(' ')[1]
     decoded_token = auth.verify_id_token(token)
+	firebaseUser = auth.get_user(decoded_token['uid']
+	if not firebaseUser.email_verified:
+		raise HTTPException(401)
